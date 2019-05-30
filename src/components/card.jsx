5c58a81817950e975e2e45ed103d59493eb99825
Adam Mattas <adamfader@gmail.com>
2019-05-29 21:18:40 -0600

    added state change for active numbers

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,21 +3,57 @@ import CardBody from "./cardBody";
 import DealButton from "./dealButton";
 
 class Card extends Component {
-  state = { numbers: [], kenoNumbers: [], bet: 0 };
+  state = {
+    numbers: [],
+    kenoNumbers: [],
+    playerNumbers: [],
+    bet: 0,
+    random: []
+  };
 
   selectNumber = (e, number) => {
     const zeroIndex = number - 1;
     let numbers = [...this.state.kenoNumbers];
     numbers[zeroIndex].selected = true;
 
+    const numberArr = [...this.state.playerNumbers];
+    numberArr.push(number);
+
     console.log("CLICK :", number, zeroIndex);
 
-    this.setState({ kenoNumbers: numbers });
+    this.setState({ kenoNumbers: numbers, playerNumbers: numberArr });
   };
 
   deal = () => {
-    let random = Math.floor(Math.random() * 80 + 1);
-    console.log("DEAL :", random);
+    let genNumbers = [];
+
+    for (let i = 0; i < 20; ) {
+      const random = Math.floor(Math.random() * 80 + 1);
+
+      const duplicate = genNumbers.indexOf(random);
+
+      if (duplicate === -1) {
+        genNumbers.push(random);
+        console.log("IIIIIII: ", i);
+        i++;
+      }
+
+      console.log("DUPLICATE CHECK: ", duplicate);
+      console.log("DEAL :", random);
+      console.log("genNumbers", genNumbers.length);
+      console.log("genNumbers", genNumbers);
+    }
+
+    const numbers = [...this.state.kenoNumbers];
+
+    genNumbers.forEach(num => {
+      console.log("NUM: ", this.state.playerNumbers.indexOf(num));
+      const duplicate = this.state.playerNumbers.indexOf(num);
+      //if (duplicate !== -1) {
+      numbers[num].active = true;
+      //}
+    });
+    this.setState({ kenoNumbers: numbers, random: genNumbers });
   };
 
   listNumbers(start, end) {
