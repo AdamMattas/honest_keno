4c67e433cd71d9028afd41e0c2dfb0fcbba1ff47
Adam Mattas <adamfader@gmail.com>
2019-07-10 20:46:59 -0600

    started keno ball transitions

--- src/components/kenoBall.jsx
+++ src/components/kenoBall.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-const KenoBall = props => {
-  console.log("RANDOM: ", props);
-  return (
-    <div className={`ball ball-${props.index + 1} ${props.status}`}>
-      <span>{props.number}</span>
-    </div>
-  );
-};
+class KenoBall extends Component {
+  state = { index: null };
+
+  componentDidMount() {
+    // const index = 0;
+    // this.setState({ index });
+    console.log("KENOBALL PROPS", this.props);
+    const index = this.props.index + 1;
+    // this.setState({ index });
+    setTimeout(() => {
+      this.setState({ index });
+    }, 0);
+  }
+
+  componentDidUpdate() {
+    console.log("UPDATE", this.state.index);
+    // const index = this.props.index + 1;
+    // if (index !== this.state.index) this.setState({ index });
+  }
+
+  render() {
+    //console.log("RANDOM: ", this.props);
+    // const delay2 = setTimeout(() => {
+    //   return this.props.index + 1;
+    // }, 3000);
+    return (
+      <div className={`ball ball-${this.state.index} ${this.props.status}`}>
+        <span>{this.props.number}</span>
+      </div>
+    );
+  }
+}
 
 export default KenoBall;
