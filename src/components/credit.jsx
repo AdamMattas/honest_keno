1364c691f5544da15feeceeec8fc26d0a4530c9a
adam <adamfader@gmail.com>
2019-12-26 22:00:09 -0700

    truncated decimals to make credit appear to be a whole number

--- src/components/credit.jsx
+++ src/components/credit.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const Credit = props => {
   console.log("TYPE: ", props.type);
+  console.log("CREDIT: ", props.credit);
   const equation = props.credit.toFixed(2);
   const credit =
-    props.type === "dollar" ? equation : props.credit / props.denom;
+    props.type === "dollar" ? equation : Math.trunc(props.credit / props.denom);
   return (
     <div className="credit-text" onClick={props.toggle}>
       {`${props.type === "dollar" ? "$" : ""}${credit}`}
