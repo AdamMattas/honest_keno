4542ded2d73596d1819be7d49073323e87407f8b
adam <adamfader@gmail.com>
2020-03-15 12:13:24 -0600

    trying to add google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import ReactGA from "react-ga";
+//import { createBrowserHistory } from "history";
 // import logo from "./logo.svg";
 import Navbar from "./components/navBar";
 import Background from "./components/background";
 import Machine from "./components/machine";
 import "./App.scss";
+
+ReactGA.initialize("159342205");
+
+//const history = createBrowserHistory();
+
+// useEffect(() => {
+// Initialize google analytics page view tracking
+// history.listen(location => {
+//   ReactGA.set({ page: location.pathname }); // Update the user's current page
+//   ReactGA.pageview(location.pathname); // Record a pageview for the given page
+// });
+// });
 
 class App extends Component {
   render() {
