a7830ea0750f2f3e154c3b9cffe3621945bb95bd
adam <adamfader@gmail.com>
2020-03-15 12:28:35 -0600

    trying to add google analytics

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import ReactGA from "react-ga";
-//import { createBrowserHistory } from "history";
+import { createBrowserHistory } from "history";
 // import logo from "./logo.svg";
 import Navbar from "./components/navBar";
 import Background from "./components/background";
 import Machine from "./components/machine";
 import "./App.scss";
 
-ReactGA.initialize("159342205");
+const App = () => {
+  ReactGA.initialize("UA-159342205-1");
 
-//const history = createBrowserHistory();
+  const history = createBrowserHistory();
 
-// useEffect(() => {
-// Initialize google analytics page view tracking
-// history.listen(location => {
-//   ReactGA.set({ page: location.pathname }); // Update the user's current page
-//   ReactGA.pageview(location.pathname); // Record a pageview for the given page
-// });
-// });
+  useEffect(() => {
+    //Initialize google analytics page view tracking
+    history.listen(location => {
+      ReactGA.set({ page: location.pathname }); // Update the user's current page
+      ReactGA.pageview(location.pathname); // Record a pageview for the given page
+    });
+  });
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar />
-        <Background />
-        <Machine />
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Background />
+      <Machine />
+    </React.Fragment>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Navbar />
+//         <Background />
+//         <Machine />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default App;
